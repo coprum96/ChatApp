8d18c7dcdb09ee43958af94758d99cfe939eb265
coprum96 <medn@list.ru>
2023-03-02 20:13:45 +0300

    new

---
+++ public/chat.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Make connection
+    const socket = io.connect('http://localhost:3000');
+  
+    // Get references to DOM elements
+    const message = document.querySelector('#message');
+    const username = document.querySelector('#username');
+    const send_message = document.querySelector('#send_message');
+    const send_username = document.querySelector('#send_username');
+    const chatroom = document.querySelector('#chatroom');
+    const feedback = document.querySelector('#feedback');
+  
+    // Emit message
+    send_message.addEventListener('click', () => {
+      socket.emit('new_message', { message: message.value });
+    });
+  
+    // Listen on new_message
+    socket.on('new_message', (data) => {
+      feedback.innerHTML = '';
+      message.value = '';
+      chatroom.innerHTML += "<p class='message'>" + data.username + ": " + data.message + '</p>';
+    });
+  
+    // Emit a username
+    send_username.addEventListener('click', () => {
+      socket.emit('change_username', { username: username.value });
+    });
+  
+    // Emit typing
+    message.addEventListener('keypress', () => {
+      socket.emit('typing');
+    });
+  
+    // Listen on typing
+    socket.on('typing', (data) => {
+      feedback.innerHTML = "<p><i>" + data.username + ' is typing a message...</i></p>';
+    });
+  });
+  
+  
